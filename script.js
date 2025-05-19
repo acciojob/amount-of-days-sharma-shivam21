a318ae02dfe6bd8cc59a4094af55feed236f7f5e
sharma-shivam21 <s3860762@gmail.com>
2025-05-19 15:44:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  return ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) ? 366 : 365;
+}
+console.log(daysOfAYear(2022)); // 365
+console.log(daysOfAYear(2024)); // 366
+console.log(daysOfAYear(1900)); // 365
+console.log(daysOfAYear(2000)); // 366
+console.log(daysOfAYear(1600)); // 366
+console.log(daysOfAYear(2100)); // 365
